84f658f0a87878a2a46279a6691e4901af562f8e
Chrisjuunor <chinonsoigweic@gmail.com>
2025-03-02 00:14:06 +0100

    edit movie fields

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -3,7 +3,15 @@ import {
   createMovie,
   getAllMovies,
   getMovieById,
+  updateMovieById,
 } from "../models/movies.model";
+
+interface movieFields {
+  title: string;
+  description: string;
+  duration: string;
+  genre: string;
+}
 
 export const addMovie = async (req: Request, res: Response): Promise<void> => {
   const { title, description, duration, genre } = req.body;
@@ -63,5 +71,50 @@ export const getMovies = async (req: Request, res: Response) => {
   } catch (err: any) {
     console.error(`Error retrieving movies ${err}`);
     res.status(500).json({ message: "Unable to get movies!" });
+  }
+};
+
+export const updateMovie = async (
+  req: Request<{ id: string }, {}, movieFields>,
+  res: Response
+) => {
+  const id = parseInt(req.params.id as string);
+  if (isNaN(id)) {
+    res.status(400).json({ message: "Invalid movie ID!" });
+    return;
+  }
+
+  const { title, description, duration, genre } = req.body;
+
+  try {
+    if (!title || !description || !duration || !genre) {
+      res.status(400).json({ message: "Fields cannot be empty" });
+      return;
+    }
+
+    //check if movie with id exists in the db
+    const movie = await getMovieById(id);
+    if(!movie){
+        console.log(`Movie with id ${id} not found in the database!`);
+        res.status(404).json({message: `Movie with id ${id} not found!`});
+        return;
+    }
+    
+    const update = await updateMovieById(id, {
+      title,
+      description,
+      duration,
+      genre,
+    });
+    console.log(update);
+    if (!update) {
+      res.status(400).json({ message: `Error updating by with id ${id}` });
+      return;
+    }
+
+    res.status(200).json(update);
+  } catch (err: any) {
+    console.error(`Error updating movie ${err}`);
+    res.status(500).json({ message: "Unable to update movie!" });
   }
 };
