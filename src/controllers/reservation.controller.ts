ff76b9bbec090b7f1e6dd75669e7c457bd2ed0af
Chrisjuunor <chinonsoigweic@gmail.com>
2025-03-14 07:02:35 +0100

    implement get reservation method

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { createReservation } from "../models/reservations.model";
+import {
+  createReservation,
+  getReservation,
+} from "../models/reservations.model";
 
 export const makeReservation = async (req: Request, res: Response) => {
   const { user_id, showtime_id, seat_number, status } = req.body;
@@ -24,5 +27,29 @@ export const makeReservation = async (req: Request, res: Response) => {
   } catch (err: any) {
     console.error("error making reservation", err);
     res.status(500).json({ message: "Error making reservation!" });
+  }
+};
+
+export const viewReservation = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  const id = parseInt(req.params.id as string);
+  if (isNaN(id)) {
+    res.status(400).json({ message: "Invalid reservation id" });
+    return;
+  }
+
+  try {
+    const reservation = await getReservation(id);
+    if (!reservation) {
+      res.status(404).json({ message: "Reservation not found!" });
+      return;
+    }
+
+    res.status(200).json(reservation);
+  } catch (err: any) {
+    console.error("Error getting reservation", err);
+    res.status(500).json({ message: "Error getting reservation!" });
   }
 };
--- src/models/reservations.model.ts
+++ src/models/reservations.model.ts
@@ -10,7 +10,7 @@ type Reservation = {
 
 export const createReservation = async (
   reservation: Omit<Reservation, "id">
-) => {
+): Promise<Reservation> => {
   try {
     const query = {
       text: "INSERT INTO reservations (user_id, showtime_id, seat_number, status) VALUES ($1, $2, $3, $4) RETURNING *",
@@ -30,14 +30,21 @@ export const createReservation = async (
   }
 };
 
-export const getReservation = async (id: number) => {
+export const getReservation = async (
+  id: number
+): Promise<Reservation | null> => {
   try {
     const query = {
-      text: "SELECT * FROM reservation WHERE id = $1",
-      value: [id],
+      text: "SELECT * FROM reservations WHERE id = $1",
+      values: [id],
     };
 
     const result = await db.query(query);
+    if (result.rows.length === 0) {
+      console.log("No reservation found for id:", id);
+      return null;
+    }
+
     return result.rows[0];
   } catch (err: any) {
     console.error("error getting reservation", err);
